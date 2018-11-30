c0b3a8b99deb8e1db5f6defcd66138b63f2ee777
boribap <bsww201@naver.com>
2018-11-30 15:58:18 +0900

    [#2] DB에 회원가입 정보 저장 완료

--- Local_Login/app/src/main/java/com/bsww201/localtoken/localtoken/SignUpActivity.kt
+++ Local_Login/app/src/main/java/com/bsww201/localtoken/localtoken/SignUpActivity.kt
@@ -3,16 +3,28 @@ package com.bsww201.localtoken.localtoken
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.telecom.Call
+import android.util.Log
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import okhttp3.ResponseBody
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
 import java.util.regex.Pattern
 
 class SignUpActivity:AppCompatActivity() {
 
-    private var nameEditText:String? = null
-    private var emailEditText:String? = null
-    private var passwordEditText:String? = null
+    private var nameEditText:String = ""
+    private var emailEditText:String = ""
+    private var passwordEditText:String = ""
+
+    // retrofit : 서버로 전달할 데이터의 클래스 객체 생성
+    private var user : User = User()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +42,33 @@ class SignUpActivity:AppCompatActivity() {
             else{
                 // 회원가입 성공 --> 정보를 서버로 전달
                 Toast.makeText(this,"회원가입 성공", Toast.LENGTH_SHORT).show()
+                Log.e("회원가입 정보 : " , nameEditText + " " + emailEditText + " " + passwordEditText)
+
+                // retrofit : --> 정보를 서버로 전달
+                var baseurl = "http://172.20.10.2:7260/"
+                var retrofit : Retrofit = Retrofit.Builder()
+                        .baseUrl(baseurl)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+
+                var serviceApi : ServiceApi = retrofit.create(ServiceApi::class.java)
+                user = user.User(nameEditText, emailEditText, passwordEditText)
+                var call : retrofit2.Call<Response<String>> = serviceApi.signUpData(user)
+
+                call.enqueue(object : Callback<Response<String>>{
+                    override fun onResponse(call: retrofit2.Call<Response<String>>?, response: Response<Response<String>>?) {
+                        Log.e("retrofit 들어옴", "retrofit 들어옴")
+
+                        if (response!!.isSuccessful()){
+                            var result : String = response?.body().toString()
+                            Log.e("결과 값", result)
+                        }
+                    }
+
+                    override fun onFailure(call: retrofit2.Call<Response<String>>?, t: Throwable?) {
+                        Log.e("에러 남", t.toString())
+                    }
+                })
 
             }
         }
@@ -38,6 +77,15 @@ class SignUpActivity:AppCompatActivity() {
             val loginIntent = Intent(this, MainActivity::class.java)
             startActivity(loginIntent)
         }
+    }
+
+    // retrofit : API 설정을 위한 인터페이스
+    public interface ServiceApi {
+
+        @POST("sign_up")
+        fun signUpData(
+                @Body user: User
+        ): retrofit2.Call<Response<String>>
     }
 
     private fun checkNameForm(name : String?) : Boolean {
@@ -83,5 +131,33 @@ class SignUpActivity:AppCompatActivity() {
         }
 
         return true
+    }
+}
+
+// retrofit : 서버로 전달할 데이터의 클래스
+class User {
+
+    var user_name : String? = null
+    var user_email : String? = null
+    var user_password : String? = null
+
+    public fun User() {
+
+    }
+
+    override fun toString(): String {
+        return "User{" +
+                "user_name=" + user_name + '\'' +
+                "user_email=" + user_email + '\'' +
+                "user_password=" + user_password + '\'' +
+                '}'
+    }
+
+    public fun User(user_name : String, user_email : String, user_password : String) : User{
+        this.user_name = user_name
+        this.user_email = user_email
+        this.user_password = user_password
+
+        return this
     }
 }
